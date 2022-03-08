d45d91384a3571a87dfdccccfccbb32084572a5b
Soumyadeep-ai <soumyadeeppal2001@gmail.com>
2022-03-09 01:12:14 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    // your firebase config
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
